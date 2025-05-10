54e61e16032bacc199bcac1b3f6fb04d0e545d8e
Asier <97402868+parzival-von@users.noreply.github.com>
2025-05-10 14:24:06 +0200

    Botones de cambio de escena funcionan

---
+++ Assets/Scripts/UI/VRButton.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class VRButton : MonoBehaviour
+{
+    public enum ButtonAction
+    {
+        LoadProfile,
+        LoadExplore,
+        LoadTrain,
+        LoadMainMenu,
+        QuitGame
+    }
+
+    public ButtonAction action;
+
+    public void OnPress()
+    {
+        UIManager ui = FindObjectOfType<UIManager>();
+        if (ui == null)
+        {
+            Debug.LogError("UIManager no trobat a l'escena.");
+            return;
+        }
+
+        switch (action)
+        {
+            case ButtonAction.LoadProfile:
+                ui.LoadProfile();
+                break;
+            case ButtonAction.LoadExplore:
+                ui.LoadExplore();
+                break;
+            case ButtonAction.LoadTrain:
+                ui.LoadTrain();
+                break;
+            case ButtonAction.LoadMainMenu:
+                ui.LoadMainMenu();
+                break;
+            case ButtonAction.QuitGame:
+                ui.QuitGame();
+                break;
+        }
+    }
+}
